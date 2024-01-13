e8a0d96d58750b66ea1529e65a030d31e7af0f2b
Ruan <ruanrln@gmail.com>
2024-01-13 01:58:38 +0000

    add strategy to compute price for a single plan and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+### Description
+
+This project is intended to read the HDF file provided by ESB networks about my smart meter so i can compute how much a period would have cost with different energy plans
+
+Did it need to be done in rust? No, but I was wanting to increase my Rust exposure
+Do I have future plans for this project? Maybe providing this as an API so others can use, or providing to somethink like bonkers or switcher so they can provide user based optimal price plans
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
 use anyhow::{Ok, Result};
 use chrono::NaiveDateTime;
 use serde::Deserialize;
-use std::{fs::File, io::BufReader};
+use std::{fs::File, io::BufReader, ops::Add};
+
+// Defines the signature for the function to define the price for a single datapoint for a plan
+trait PricePlanStrategy {
+    fn price_for_singe_period(&self, datapoint: &SmartMeterData) -> EnergyBillEntry;
+    fn standing_charge_per_day(&self) -> EnergyBillEntry;
+}
+
+fn compute_total_bill_for_perio<T>(
+    datapoints: &Vec<SmartMeterData>,
+    price_plan: &T,
+) -> EnergyBillEntry
+where
+    T: PricePlanStrategy + Sized,
+{
+    datapoints
+        .iter()
+        .fold(EnergyBillEntry::Debit(0.0), |acc, d| {
+            let price_for_singe_period = price_plan.price_for_singe_period(d);
+            let energy_bill_entry = acc + price_for_singe_period;
+            println!("{acc:?} + {price_for_singe_period:?} = {energy_bill_entry:?}");
+            energy_bill_entry
+        })
+}
+
+struct ElectricIrelandHomeElectric14;
+impl PricePlanStrategy for ElectricIrelandHomeElectric14 {
+    fn price_for_singe_period(&self, datapoint: &SmartMeterData) -> EnergyBillEntry {
+        match datapoint.read_type {
+            SmartMeterDataType::ActiveImport => {
+                EnergyBillEntry::Debit(0.3895 * (1.0 - 0.14) * datapoint.read_value)
+            }
+            SmartMeterDataType::ActiveExport => {
+                EnergyBillEntry::Credit(0.21 * datapoint.read_value)
+            }
+        }
+    }
+
+    fn standing_charge_per_day(&self) -> EnergyBillEntry {
+        EnergyBillEntry::Debit(272.61 / 365f32)
+    }
+}
 
 fn smart_meter_datetime_desserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
 where
@@ -34,6 +75,41 @@ enum SmartMeterDataType {
     ActiveExport,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum EnergyBillEntry {
+    Credit(f32),
+    Debit(f32),
+}
+
+impl Add for EnergyBillEntry {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (EnergyBillEntry::Debit(self_value), EnergyBillEntry::Debit(rhs_value)) => {
+                EnergyBillEntry::Debit(self_value + rhs_value)
+            }
+            (EnergyBillEntry::Credit(self_value), EnergyBillEntry::Credit(rhs_value)) => {
+                EnergyBillEntry::Credit(self_value + rhs_value)
+            }
+            (EnergyBillEntry::Credit(self_value), EnergyBillEntry::Debit(rhs_value)) => {
+                if self_value > rhs_value {
+                    EnergyBillEntry::Credit(self_value - rhs_value)
+                } else {
+                    EnergyBillEntry::Debit(rhs_value - self_value)
+                }
+            }
+            (EnergyBillEntry::Debit(self_value), EnergyBillEntry::Credit(rhs_value)) => {
+                if rhs_value > self_value {
+                    EnergyBillEntry::Credit(rhs_value - self_value)
+                } else {
+                    EnergyBillEntry::Debit(self_value - rhs_value)
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, Deserialize)]
 struct SmartMeterData {
     //format:
@@ -54,18 +130,17 @@ struct SmartMeterData {
     read_data_and_end_time: NaiveDateTime,
 }
 
-
 fn main() -> Result<()> {
     const FILENAME: &str = "data/HDF_reduced.csv";
     let f = File::open(FILENAME)?;
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
         .from_reader(reader);
     let data: Vec<SmartMeterData> = rdr.deserialize().flat_map(|x| x).collect();
-    for item in data {
-        println!("{item:?}");
-    }
+
+    let total = compute_total_bill_for_perio(&data, &ElectricIrelandHomeElectric14);
+    println!("{total:?}");
     Ok(())
 }
